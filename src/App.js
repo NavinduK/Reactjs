f5f7f4aab14954405456ee820878dddfdd065392
navinduk <navindu.nk@gmail.com>
2020-05-25 13:45:09 +0530

    remove node_module files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Covid-19
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      Navindu Kavishka
     </div>
   );
 }
