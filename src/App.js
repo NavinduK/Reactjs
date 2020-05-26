7abd28642c2426ed57b50ba39cb718cb41436060
navinduk <navindu.nk@gmail.com>
2020-05-26 22:59:40 +0530

    Commit 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import Global from './components/Global';
-
+import axios from 'axios';
 
 function App() {
+
+  const[latest, setLatest] = useState("");
+
+  useEffect(()=>{
+    axios
+      .get("https://hpb.health.gov.lk/api/get-current-statistical")
+      .then(res=>{
+        setLatest(res.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+  }, []);
+
   return(
     <div className="App">
-        <Global/>
-        
+        <Global latest={latest}/>
     </div>
   );
 }
--- src/components/Global.jsx
+++ src/components/Global.jsx
@@ -5,15 +5,23 @@ class Global extends Component {
         return (
             <div>
                 <div className="section-global center-text">
-                    <div className="card text-white bg-dark mb-3 " style={{width: '22%', margin:'1.5%'}}>
+                    <div className="card bg-dark mb-3 " style={{width: '22%', margin:'1.5%'}}>
                         <div className="card-header"><h2>Total Cases</h2></div>
                         <div className="card-body">
-                            <h3 className="card-title">100</h3>
+                            <h3 className="card-title">{this.props.latest.message}</h3>
                             <p className="card-text">Last updated at:</p>
                         </div>
                     </div>
 
-                    <div className="card text-white bg-dark mb-3" style={{width: '22%', margin:'1.5%'}}>
+                    <div className="card bg-dark mb-3" style={{width: '22%', margin:'1.5%'}}>
+                        <div className="card-header"><h2>New Cases</h2></div>
+                        <div className="card-body">
+                            <h3 className="card-title">{this.props.latest.global_new_cases}</h3>
+                            <p className="card-text">Last updated at:</p>
+                        </div>
+                    </div>
+
+                    <div className="card bg-dark mb-3" style={{width: '22%', margin:'1.5%'}}>
                         <div className="card-header"><h2>Deaths</h2></div>
                         <div className="card-body">
                             <h3 className="card-title">100</h3>
@@ -21,16 +29,8 @@ class Global extends Component {
                         </div>
                     </div>
 
-                    <div className="card text-white bg-dark mb-3" style={{width: '22%', margin:'1.5%'}}>
+                    <div className="card bg-dark mb-3" style={{width: '22%', margin:'1.5%'}}>
                         <div className="card-header"><h2>Recovered</h2></div>
-                        <div className="card-body">
-                            <h3 className="card-title">100</h3>
-                            <p className="card-text">Last updated at:</p>
-                        </div>
-                    </div>
-
-                    <div className="card text-white bg-dark mb-3" style={{width: '22%', margin:'1.5%'}}>
-                        <div className="card-header"><h2>Suspected</h2></div>
                         <div className="card-body">
                             <h3 className="card-title">100</h3>
                             <p className="card-text">Last updated at:</p>
